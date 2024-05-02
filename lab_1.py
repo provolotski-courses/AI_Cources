08623e27c78464b564d212b1ba44c91c9d84bc0e
provolotski <provolotski@gmail.com>
2024-05-02 19:07:39 +0300

    first lab

---
+++ lab_1.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+myDict = {'Author': input('Input name of author: '), 'Title': input('Input title of book: ')}
+flag = True
+while flag:
+    try:
+        myDict['Year'] = int(input('Input publication year: '))
+        flag = myDict['Year'] < 1454 or myDict['Year'] > date.today().year
+    except ValueError:
+        pass
+    finally:
+        if not flag:
+            print('Wrong year')
+print(myDict)
